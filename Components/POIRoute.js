c92a390094e41ea7202fe8568b973c7c38bbc070
Guilliee <73161187+Guilliee@users.noreply.github.com>
2021-06-09 23:14:58 +0200

    MapViewDirections commented, app.json needs new key from google cloud console. Test new key with postman @https://maps.googleapis.com/maps/api/directions/json?origin=Disneyland&destination=Universal+Studios+Hollywood&key=YOUR_API_KEY

--- Components/POIRoute.js
+++ Components/POIRoute.js
@@ -1,37 +1,44 @@
 import React, {useState, useEffect} from 'react';
 import {locations} from "./POIdata"
-import MapViewDirections from 'react-native-maps-directions';
+// import MapViewDirections from 'react-native-maps-directions';
+import MapView, {Callout, Marker, Polyline,} from "react-native-maps";
 
-const origin = {latitude: 52.226632, longitude: 5.180905};
-const destination = {latitude: 52.224442, longitude: 5.181236};
-const GOOGLE_MAPS_APIKEY = 'AIzaSyAX3EVsoKVc0xTSnp3SZhtkWWP-ykkt3CA';
+import {Platform, Text, View, StyleSheet, Dimensions} from 'react-native';
+import * as Location from 'expo-location';
+import {Image} from "react-native";
 
-const POIRoute = () => {
-    return (
-        locations.map(() => (
-            <MapViewDirections
-                origin={origin}
-                destination={destination}
-                apikey={GOOGLE_MAPS_APIKEY} // google API key
-                strokeWidth={4}
-                strokeColor="#111111"
-            />
-        ))
-    );
-};
-export default POIRoute
+// const origin = {latitude: 52.226632, longitude: 5.180905};
+// const destination = {latitude: 52.224442, longitude: 5.181236};
+// const GOOGLE_MAPS_APIKEY = 'AIzaSyAX3EVsoKVc0xTSnp3SZhtkWWP-ykkt3CA';
+
 
 
 // const POIRoute = () => {
 //     return (
-//         locations.map(marker => (
-//             <Polyline key={ Math.random().toString(36).substr(2, 9) }
-//                 coordinates={locations}
-//                 strokeColor="#000"
-//                 strokeColors={['#7F0000']}
-//                 strokeWidth={6}
+//         locations.map(() => (
+//             <MapViewDirections
+//                 origin={origin}
+//                 destination={destination}
+//                 apikey={GOOGLE_MAPS_APIKEY} // google API key
+//                 strokeWidth={4}
+//                 strokeColor="#111111"
 //             />
 //         ))
 //     );
 // };
 // export default POIRoute
+
+
+const POIRoute = () => {
+    return (
+        locations.map(marker => (
+            <Polyline key={ Math.random().toString(36).substr(2, 9) }
+                coordinates={locations}
+                strokeColor="#000"
+                strokeColors={['#7F0000']}
+                strokeWidth={6}
+            />
+        ))
+    );
+};
+export default POIRoute
