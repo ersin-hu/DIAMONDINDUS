5c361f7348c64ab2f7546ba59e4d38b1babdbd9d
dpk000 <Halloyolo1!>
2021-06-10 11:39:01 +0200

    Quiz component

---
+++ Components/Quiz.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+
+import {StyleSheet, Text, TouchableOpacity, View} from "react-native";
+
+const Quiz = () => {
+    return(
+        <View style={styles.container}>
+            <View style={styles.top}>
+                <Text style={styles.question}>Wanneer was de eerste diamantbewerker?</Text>
+            </View>
+            <View style={styles.options}>
+                <TouchableOpacity style={styles.optionButton}>
+                    <Text style={styles.option}>A: 1900</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.optionButton}>
+                    <Text style={styles.option}>B: 1920</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.optionButton}>
+                    <Text style={styles.option}>C: 1950</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.optionButton}>
+                    <Text style={styles.option}>D: 2000</Text>
+                </TouchableOpacity>
+            </View>
+
+        </View>
+    );
+};
+export default Quiz;
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 12,
+        height: '100%',
+    },
+    top: {
+        marginVertical: 16,
+    },
+    options: {
+        marginVertical: 16,
+        flex: 1,
+    },
+    question:{
+        fontSize: 30,
+    },
+    option: {
+        fontSize: 18,
+        color: 'white',
+    },
+    optionButton:{
+        paddingVertical: 12,
+        marginVertical: 6,
+        backgroundColor: '#34A0A4',
+        paddingHorizontal: 12,
+        borderRadius: 12,
+
+    }
+});
